add0c5435014b15471c82dcb5a6e2af0915a1311
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-04-28 23:14:22 +0300

    schemas

---
+++ src/models/storesNearest.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+
+const storesNearestSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true },
+);
+
+export const NearestStore = model('pharmacies', storesNearestSchema);
+
+export const Store = model('pharma', storesNearestSchema);
---
+++ src/models/users.js
@@ -0,0 +1,29 @@
+import { model, Schema } from 'mongoose';
+
+const usersSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, 'Email is required'],
+    },
+    phone: {
+      type: String,
+      required: [true, 'Phone is required'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Set password for user'],
+    },
+    token: {
+      type: String,
+      default: '',
+    },
+  },
+  { versionKey: false, timestamps: true },
+);
+export const NearestStore = model('User', usersSchema, 'users');
